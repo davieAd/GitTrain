aca35e29211de160ff32cfd0589fd46afacbd535
davieAd <jeowsxnxnd@gmail.com>
2022-01-04 20:45:39 +0200

    Init2

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,8 @@ public class Main {
     public static void main(String[] args) {
         String git = "Git is Cool";
         System.out.println(git);
+
+        String git2 = "Git is SuperCool";
+        System.out.println(git2);
     }
 }
